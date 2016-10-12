6ec7e9790524ba996e2012ec7574bb978ef1dadb
Jeffrey Chan <jeffschan97@gmail.com>
2016-10-12 16:32:45 -0700

    Fixed carousel

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     var descriptions  = ['Web Developer', 'Coder', 'Electrical Engineer', 'Dancer'];
-    var descriptionIndex = 2;
-    document.getElementById('fillMe').innerHTML = descriptions[2];
+    var descriptionIndex = 3;
+    document.getElementById('fillMe').innerHTML = descriptions[3];
     
     function changeDescription() {
         descriptionIndex += 1;
-        if (descriptionIndex > 2) {
+        if (descriptionIndex > 3) {
             descriptionIndex = 0;
         }
         document.getElementById('fillMe').innerHTML = descriptions[descriptionIndex];
