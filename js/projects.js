ce6c844f26c30c5ba9820dd907910f292e2e6bd9
Jeffrey Chan <jeffschan97@gmail.com>
2016-12-23 15:55:09 -0800

    Saving just in case I mess up

--- js/projects.js
+++ js/projects.js
@@ -1,28 +1,32 @@
 $(document).ready(function () {
     // initial setting
-    if (window.location.hash == '') {
-        window.location.hash = '#webdev'; // default setting
-    }
-    $('.nav-item').removeClass('active'); // remove active class from all of them
-    $('.project-group').hide(); // hide all of them
+//    if (window.location.hash == '') {
+//        window.location.hash = '#webdev'; // default setting
+//    }
+//    $('.nav-item').removeClass('active'); // remove active class from all of them
+//    $('.project-group').hide(); // hide all of them
+//    
+//    // make correct nav items active and show correct projects
+//    switch (window.location.hash) {
+//        case '#hardware':
+//            $('.project-navbar .hardware').addClass('active');
+//            $('.hardware-projects').show();
+//            break;
+//        case '#code':
+//            $('.project-navbar .code').addClass('active');
+//            $('.code-projects').show();
+//            break;
+//        case '#webdev':
+//            $('.project-navbar .webdev').addClass('active');
+//            $('.webdev-projects').show()
+//            break;
+//    }
     
-    // make correct nav items active and show correct projects
-    switch (window.location.hash) {
-        case '#hardware':
-            $('.project-navbar .hardware').addClass('active');
-            $('.hardware-projects').show();
-            break;
-        case '#code':
-            $('.project-navbar .code').addClass('active');
-            $('.code-projects').show();
-            break;
-        case '#webdev':
-            $('.project-navbar .webdev').addClass('active');
-            $('.webdev-projects').show()
-            break;
-    }
+    $('.project-navbar .webdev').addClass('active');
+    $('.project-group').hide(); // hide all of them
+    $('.webdev-projects').show()
 
-    // Project Navbar code
+    // PROJECT NAVBAR
     $('.nav-item').click(function () {
         // injecting .active class in nav items with click
         $('.nav-item').removeClass('active');
@@ -48,16 +52,62 @@ $(document).ready(function () {
         }
     });
     
+    // PROJECT DROPDOWN CODE
+    $('.item').click(function () {
+        console.log($(this).html());
+        // displaying the correct set of projects
+        if ($(this).html() == 'Hardware') {
+            $('.hardware-projects').show();
+            $('.code-projects').hide();
+            $('.webdev-projects').hide();
+        }
+        
+        if ($(this).html() == 'Code') {
+            $('.hardware-projects').hide();
+            $('.code-projects').show();
+            $('.webdev-projects').hide();
+        }
+        
+        if ($(this).html() == 'Web Development') {
+            $('.hardware-projects').hide();
+            $('.code-projects').hide();
+            $('.webdev-projects').show();
+        }
+    });
+    
     // MEDIA LINKS FOR MOBILE
+    $('a').click(function () {
+        if ($(this).parent().parent().css("opacity") === "0") {
+            toggleOpacity($(this).parent().parent());
+            return false;
+        }
+        return true;
+    });
+    
     $('.media-links').click(function () {
+        toggleOpacity($(this));
+    }).hover(function () {
+        if ($(this).css("opacity") === "0") {
+            toggleOpacity($(this));
+        }
+    }, function () {
         if ($(this).css("opacity") === "1") {
-            $(this).css({
+            toggleOpacity($(this));
+        }
+    });
+    
+    function toggleOpacity (item) {
+        if ($(item).css("opacity") === "1") {
+            $(item).css({
                 opacity: "0"
             });
         } else {
-            $(this).css({
+            $(item).css({
                 opacity: "1"
             });
         }
-    });
+    }
+    
+    // Initialize Semantic UI Dropdown
+    $('.dropdown').dropdown();
 });
