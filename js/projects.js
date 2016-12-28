fe8bbc1c98626718aef75f43c287e8ff081f1228
Jeffrey Chan <jeffschan97@gmail.com>
2016-12-27 21:43:44 -0800

    added calculator project

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // INITIAL DISPLAY SETTINGS
     $('.nav-item.webdevelopment').addClass('active');
     $('.item.webdevelopment').addClass('selected');
-    $('.project-group').hide(); // hide all of them
+    $('.project-group').hide(); // hide all projects
     $('.webdev-projects').show()
 
     // PROJECT NAVIGATION
@@ -31,16 +31,25 @@ $(document).ready(function () {
             $('.hardware-projects').show();
             $('.code-projects').hide();
             $('.webdev-projects').hide();
+            $('.ios-projects').hide();
         }
         else if ($(this).children().html() == 'Code') {
             $('.hardware-projects').hide();
             $('.code-projects').show();
             $('.webdev-projects').hide();
+            $('.ios-projects').hide();
         }
         else if ($(this).children().html() == 'Web Development') {
             $('.hardware-projects').hide();
             $('.code-projects').hide();
             $('.webdev-projects').show();
+            $('.ios-projects').hide();
+        }
+        else if ($(this).children().html() == 'iOS') {
+            $('.hardware-projects').hide();
+            $('.code-projects').hide();
+            $('.webdev-projects').hide();
+            $('.ios-projects').show();
         }
     });
     
